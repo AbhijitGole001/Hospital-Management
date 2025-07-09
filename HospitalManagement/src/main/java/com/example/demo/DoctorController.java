e3f0f9bf6752a08e1b856ed081d0163d8d9fc013
Abhijit <abhijitgole001@gmail.com>
2025-07-09 18:49:07 +0530

    add doctor api added.

--- HospitalManagement/src/main/java/com/example/demo/DoctorController.java
+++ HospitalManagement/src/main/java/com/example/demo/DoctorController.java
@@ -1,6 +1,14 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DoctorController {
 	
 
+	String addDoctor()
+	{
+		return "doctor";
+	}
+	
 }
